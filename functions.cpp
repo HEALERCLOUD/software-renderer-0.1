c8752d96d0d77f499cd35d6a8dc8246c1ddd689c
luka akhaladze <lukaakhaladze@lukas-MacBook-Air.local>
2023-11-05 22:46:01 +0400

    first commit

---
+++ functions.cpp
@@ -0,0 +1,10 @@
+//
+// Created by luka akhaladze on 05.11.23.
+//
+#include "iostream"
+#include "functions.h"
+
+void print(int input)
+{
+    std::cout<<input;
+}
---
+++ functions.h
@@ -0,0 +1,10 @@
+//
+// Created by luka akhaladze on 05.11.23.
+//
+
+#ifndef SOFTWARE_RENERER_0_1_FUNCTIONS_H
+#define SOFTWARE_RENERER_0_1_FUNCTIONS_H
+
+#endif //SOFTWARE_RENERER_0_1_FUNCTIONS_H
+
+void print(int);
